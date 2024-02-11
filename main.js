cdd54402f32b714756cd8f1a310d7986937cb442
jafarjallad1 <146374073+jafarjallad1@users.noreply.github.com>
2024-02-11 16:21:37 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+ 
+  function increase (){
+
+    let button = this;
+    let countContainer = button.closest(".part");
+    let counter = countContainer.querySelector(".counter").textContent;
+    let total = parseInt(document.querySelector(".total").textContent);
+    total += 1 ;
+    let to =total.toString();
+    
+    let count1 = parseInt(counter);
+    count1 += 1 ;
+    let st =count1.toString();
+    countContainer.querySelector(".counter").textContent = st;
+    document.querySelector(".total").textContent = to;
+    
+ }
